dc38c6e9390f09cb129a15b58edb92999c183354
SayedAlesawy <Sayed.Kotb97@eng-st.cu.edu.eg>
2017-11-16 17:20:43 +0200

    Added all models' attributes
    Added the attributes for all models

--- db/migrate/20171116114714_base_design_model.rb
+++ db/migrate/20171116114714_base_design_model.rb
@@ -3,19 +3,40 @@ class BaseDesignModel < ActiveRecord::Migration[5.1]
 
     create_table :organizations do |t|
       #attributes
+      t.string :name,           :null => false 
+      t.text   :description,    :null => true 
+      t.string :university,     :null => false 
+      t.string :logo,           :null => true  
+      t.string :address,        :null => false 
+      t.string :phone,          :null => false 
+      t.string :email,          :null => false 
+      t.text   :event_schedule, :null => true  
       t.timestamps
+
       #relations
     end
 
     create_table :workspaces do |t|
        #attributes
+       t.string :name,   :null => false   
+       t.string :logo,   :null => true 
+       t.text   :about,  :null => true   
        t.timestamps
+
        #relations
     end
 
     create_table :branches do |t|
       #attributes
+      t.integer :id,             :null => false 
+      t.integer :numer_of_rooms, :null => false   
+      t.text    :services,       :null => true  
+      t.string  :address,        :null => false 
+      t.string  :phone,          :null => false 
+      t.string  :email,          :null => false 
+      t.string  :photos,         :null => true   
       t.timestamps
+
       #relations
       t.belongs_to :workspace ,index: { unique: true }, foreign_key: true
       
@@ -23,7 +44,13 @@ class BaseDesignModel < ActiveRecord::Migration[5.1]
 
     create_table :rooms do |t|
       #attributes
+      t.integer :id,           :null => false 
+      t.float   :price,        :null => false 
+      t.boolean :availability, :null => false  
+      t.integer :capacity,     :null => false 
+      t.text    :services,     :null => true  
       t.timestamps
+
       #relations
       t.belongs_to :branch ,index: { unique: true }, foreign_key: true
       
@@ -31,39 +58,64 @@ class BaseDesignModel < ActiveRecord::Migration[5.1]
 
     create_table :workshops do |t|
       #attributes
+      t.integer :id,          :null => false 
+      t.string  :title,       :null => false 
+      t.text    :description, :null => true  
+      t.date    :date,        :null => false 
+      t.time    :time,        :null => false 
       t.timestamps
+
       #relations
       t.belongs_to :branch ,index: { unique: true }, foreign_key: true
       t.belongs_to :organization ,index: { unique: true }, foreign_key: true
-      
       
     end
 
     create_table :events do |t|
       
       #attributes
+      t.integer :id,          :null => false 
+      t.string  :title,       :null => false 
+      t.text    :description, :null => false  
+      t.time    :time,        :null => false 
+      t.date    :date,        :null => false 
+      t.string  :venue_name,  :null => false 
+      t.string  :location,    :null => false 
+      t.text    :schedule,    :null => false 
       t.timestamps
+
       #relations
       t.belongs_to :organization ,index: { unique: true }, foreign_key: true
+      t.belongs_to :branch, index: { unique: true }, foreign_key: true 
       
     end
 
     create_table :reviews do |t|
       
       #attributes
+      t.integer :id,        :null => false 
+      t.text    :feedback,  :null => false 
+      t.float   :rating,    :null => false 
       t.timestamps
+
       #relations
       t.belongs_to :user ,index: { unique: true }, foreign_key: true
       t.belongs_to :branch ,index: { unique: true }, foreign_key: true
       t.belongs_to :workshop ,index: { unique: true }, foreign_key: true
-      
+      t.belongs_to ::organization, index { unique: true}, foreign_key: true 
       
     end
 
     create_table :reservations do |t|
       
       #attributes
+      t.integer :id,                 :null => false 
+      t.time    :start_time,         :null => false 
+      t.float   :duration,           :null => false 
+      t.boolean :payment_status,     :null => false 
+      t.integer :numer_of_attendees, :null => true  
       t.timestamps
+
       #relations
       t.belongs_to :branch ,index: { unique: true }, foreign_key: true
       t.belongs_to :user ,index: { unique: true }, foreign_key: true
@@ -74,7 +126,9 @@ class BaseDesignModel < ActiveRecord::Migration[5.1]
     create_table :tags do |t|
       
       #attributes
+      t.string :title  :null => false 
       t.timestamps
+
       #relations
 
     end
