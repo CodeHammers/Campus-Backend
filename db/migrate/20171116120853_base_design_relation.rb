57e9c6b282b1b02cfb739771c93bf7b056dfd4cd
SayedAlesawy <Sayed.Kotb97@eng-st.cu.edu.eg>
2017-11-16 17:52:12 +0200

    Added all attributes for relation tables

--- db/migrate/20171116120853_base_design_relation.rb
+++ db/migrate/20171116120853_base_design_relation.rb
@@ -2,7 +2,10 @@ class BaseDesignRelation < ActiveRecord::Migration[5.1]
   def change
     create_table :positions do |t|
       #attributes
+      t.time  :start_time :null => false  
+      t.float :salary     :null => true 
       t.timestamps
+
       #relations
       t.belongs_to :branch ,index: { unique: true }, foreign_key: true
       t.belongs_to :user ,index: { unique: true }, foreign_key: true
@@ -13,6 +16,7 @@ class BaseDesignRelation < ActiveRecord::Migration[5.1]
     create_table :attends do |t|
       #attributes
       t.timestamps
+
       #relations
       t.belongs_to :event ,index: { unique: true }, foreign_key: true
       t.belongs_to :user ,index: { unique: true }, foreign_key: true
@@ -22,11 +26,24 @@ class BaseDesignRelation < ActiveRecord::Migration[5.1]
     create_table :subscribes do |t|
       #attributes
       t.timestamps
+
        #relations
       t.belongs_to :user ,index: { unique: true }, foreign_key: true
       t.belongs_to :organization ,index: { unique: true }, foreign_key: true
        
     end
 
+    create_table :identified_by do |t|
+      #attributes
+      t.string :title  :null => false
+
+      #relations
+      t.belongs_to :organization, index { unique: true }, foreign_key: true
+      t.belongs_to :workshop, index { unique: true }, foreign_key: true
+      t.belongs_to :event, index { unique: true }, foreign_key: true 
+
+    end
+
+    #TODO: create table for the instructs/speaks relationship
   end
 end
