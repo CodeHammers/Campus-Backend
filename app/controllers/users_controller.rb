31209f8b7c555c1c4ee0fbdf24cbe0086cd468b5
Kareem-Emad <kareememad400@gmail.com>
2017-12-20 01:05:49 +0200

    bypassing sec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1
   def update
-    if @user.update(user_params)
+    if @user.update(password: user_params[:pd])
       render json: @user
     else
       render json: @user.errors, status: :unprocessable_entity
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:email,:password)
+      params.require(:user).permit(:email,:pd)
     end
 end
