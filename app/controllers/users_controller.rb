07e1b65365e53073bc343d1516ea8588a095c612
Kareem-Emad <kareememad400@gmail.com>
2017-12-20 16:53:06 +0200

    u auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:email,:pd)
+      params.require(:user).permit(:email,:password,:nickname,:image)
     end
 end
