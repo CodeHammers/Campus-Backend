c08cfdafa8eb8ae143b85bc7d725b87982977a8d
Kareem-Emad <kareememad400@gmail.com>
2017-12-20 02:50:38 +0200

    sql param change for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -10,25 +10,26 @@ class Review < ApplicationRecord
 
     #A function to retrive a certain review (full retrieval of data)
     def self.get_review(review_id)
-        review = Review.execute_sql("select * from reviews as r where r.id = ?",review_id)
+        review = Review.execute_sql("select r.feedback , r.rating,r.created_at,u.name , u.image from reviews as r, users as u where r.id = ? and r.user_id =u.id",review_id)
+
         return review
     end
 
     #A function to retrieve all reviews made to a workspace branch (only relevant data)
     def self.get_all_branch_reviews(branch_id)
-        reviews = Review.execute_sql("select r.id, r.feedback, r.rating, r.created_at, r.user_id from reviews as r where r.branch_id = ?", branch_id).to_a
+        reviews = Review.execute_sql("select r.feedback , r.rating,r.created_at,u.name , u.image  from reviews as r ,users as u where r.branch_id = ? and r.user_id =u.id", branch_id).to_a
         return reviews
     end
 
     #A function to retrieve all reviews made to a workshop (only relevant data)
     def self.get_all_workshop_reviews(workshop_id)
-        reviews = Review.execute_sql("select r.id, r.feedback, r.rating, r.created_at, r.user_id from reviews as r where r.workshop_id = ?", workshop_id).to_a
+        reviews = Review.execute_sql("select r.feedback , r.rating,r.created_at,u.name , u.image  from reviews as r ,users as u where r.workshop_id = ? and r.user_id =u.id", workshop_id).to_a
         return reviews
     end
 
     #A function to retrieve all reviews made to an organization (only relevant data)
     def self.get_all_organization_reviews(organization_id)
-        reviews = Review.execute_sql("select r.id, r.feedback, r.rating, r.created_at, r.user_id from reviews as r where r.organization_id = ?", organization_id).to_a
+        reviews = Review.execute_sql("select r.feedback , r.rating,r.created_at,u.name , u.image  from reviews as r , users as u where r.organization_id = ? and r.user_id =u.id", organization_id).to_a
         return reviews
     end
 
